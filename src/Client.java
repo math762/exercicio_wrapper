ed757cbc69ac9ee99989e033692ea9cf9ecbbe81
Matis762 <matheus.liduvino762@gmail.com>
2023-01-28 13:27:16 -0300

    Exercio proposto sobra castign

---
+++ src/Client.java
@@ -0,0 +1,26 @@
+public class Client {
+    private Integer id;
+    private String name;
+    private String lastName;
+    private Integer age;
+    private Boolean isNiga;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public boolean getIsNiga(){
+        return isNiga;
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,11 @@
+public class Program {
+    public static void main(String[] args) {
+        int id = 5595;
+        Integer id2 = id ;
+//        Casting implicito;
+
+        System.out.println(id2.toString() + "Maçsfsafs");
+
+    }
+}
+
